9df8768933c9ee7acdccf8ff575ed47ceb28f9ce
Mary Gargas <marygargas13@gmail.com>
2025-06-15 18:14:01 +0300

    final before rebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// src/index.js أو src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// ✅ استدعاء ملفات الستايل
+import './styles/reset.css';
+import './styles/utilities.css';
+import './styles/App.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
